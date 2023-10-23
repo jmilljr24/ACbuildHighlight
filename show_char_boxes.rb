f044b7bdbb598cd0bb5c6d23d4333ceb77ad5499
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-23 12:25:48 -0400

    Change color selection to sample

--- show_char_boxes.rb
+++ show_char_boxes.rb
@@ -12,7 +12,7 @@ class ShowTextProcessor < HexaPDF::Content::Processor
     super()
     # @text_list_arr = text_list
     @canvas = page.canvas(type: :overlay)
-    @parts = %w[the F1006C F1012A F1006D F1006B F1032L F1011C F1029L F1010C F1010A F1011E F1012E F1010C F1010 F1011 F1011A
+    @parts = %w[F1006C F1012A F1006D F1006B F1032L F1011C F1029L F1010C F1010A F1011E F1012E F1010C F1010 F1011 F1011A
                 F1010CR F1010CL F1007L]
     @colors = %w[violet yellow maroon brown cyan darkgreen darkred gold deeppink olivedrab paleturquoise red green blue
                  orange]
@@ -32,7 +32,7 @@ class ShowTextProcessor < HexaPDF::Content::Processor
     return unless @parts.include?(part) # do nothing if part is not on current page
 
     # check if part was on previous page
-    @color_key[part] = @prev_color.delete(part) if !@prev_parts.nil? and @prev_parts.include?(part)
+    @color_key[part] = @prev_color.delete(part) if !@prev_parts.nil? and @prev_parts.include?(part) # rubocop:disable Style/AndOr
 
     key_color(part, str) unless @color_key.key?(part) # return if part/color pair is in hash
 
@@ -56,7 +56,9 @@ class ShowTextProcessor < HexaPDF::Content::Processor
   end
 
   def key_color(part, str)
-    color = @colors.pop
+    # color = @colors.pop
+    n = @color_key.values
+    p color = (@colors - n).sample
     @color_key[part] = [color, str]
   end
   alias show_text_with_positioning show_text
@@ -68,7 +70,7 @@ doc = HexaPDF::Document.open(ARGV.shift)
 doc.pages.each_with_index do |page, index|
   puts "Processing page #{index + 1}"
   unless @prev_color.nil?
-    p @prev_parts = @prev_color.keys
+    @prev_parts = @prev_color.keys
     # @prev_color.each do |_key, _value|
     #   parts.each do |part|
     #     p part
@@ -79,6 +81,7 @@ doc.pages.each_with_index do |page, index|
   processor = ShowTextProcessor.new(page, index, @prev_parts, @prev_color)
   # binding.pry
   page.process_contents(processor)
+  @prev_color&.clear #  clear if not nil
   @prev_color = processor.color_key
 end
 doc.write('show_char_boxes.pdf', optimize: true)
