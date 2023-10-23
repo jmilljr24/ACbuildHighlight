1056c5461117e019d64014146634e3b7f70fda76
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-23 19:01:09 -0400

    Move text highlighting from show_text method

--- show_char_boxes.rb
+++ show_char_boxes.rb
@@ -7,8 +7,9 @@ require 'hexapdf'
 
 class ShowTextProcessor < HexaPDF::Content::Processor
   attr_accessor :page_number, :color_key, :prev_parts, :prev_color
+  attr_reader :boxes
 
-  def initialize(page, page_number, prev_parts = nil, prev_color = nil, color_key = {}) # text_list = []
+  def initialize(page, page_number, prev_parts = nil, prev_color = nil, color_key = {})
     super()
     # @text_list_arr = text_list
     @canvas = page.canvas(type: :overlay)
@@ -21,11 +22,13 @@ class ShowTextProcessor < HexaPDF::Content::Processor
     @page_number = page_number
     @color_key['Page Number'] = @page_number
     @used_colors = []
+    @boxes = []
+    @part = nil
   end
 
-  def show_text(str) # rubocop:disable Metrics/CyclomaticComplexity
+  def show_text(str)
     begin
-      p part = str.scan(/\w+/).join # Converts utf-8 str to text string
+      part = str.scan(/\w+/).join # Converts utf-8 str to text string
     rescue StandardError
       puts 'invalid string'
     end
@@ -36,14 +39,14 @@ class ShowTextProcessor < HexaPDF::Content::Processor
 
     key_color(part, str) unless @color_key.key?(part) # return if part/color pair is in hash
 
-    boxes = decode_text_with_positioning(@color_key.dig(part, 1)) # set boxes to str in color hash
-    return if boxes.string.empty?
+    @boxes << decode_text_with_positioning(@color_key.dig(part, 1)) # set boxes to str in color hash
+    # return if @boxes.string.empty?
 
-    begin
-      box_fill(boxes, @color_key.dig(part, 0))
-    rescue StandardError
-      box_fill(boxes, 'yellow') # set yellow if all colors used already
-    end
+    # begin
+    #   box_fill(@boxes, @color_key.dig(part, 0))
+    # rescue StandardError
+    #   box_fill(@boxes, 'yellow') # set yellow if all colors used already
+    # end
   end
 
   def box_fill(boxes, color)
@@ -66,6 +69,22 @@ class ShowTextProcessor < HexaPDF::Content::Processor
     color = @used_colors.empty? ? @colors.sample : (@colors - @used_colors).sample
     @color_key[part] = [color, str]
   end
+
+  def text_highlight
+    @boxes.each do |text_box|
+      part = []
+      i = 0
+      text_box.each do
+        part << text_box[i].string
+        i += 1
+      end
+      begin
+        box_fill(text_box, @color_key.dig(part.join, 0))
+      rescue StandardError
+        box_fill(text_box, 'yellow') # set yellow if all colors used already
+      end
+    end
+  end
   alias show_text_with_positioning show_text
 end
 @prev_color = nil
@@ -82,5 +101,6 @@ doc.pages.each_with_index do |page, index|
   page.process_contents(processor)
   @prev_color&.clear #  clear if not nil
   @prev_color = processor.color_key
+  processor.text_highlight
 end
 doc.write('show_char_boxes.pdf', optimize: true)
