d47ac4216adf401dfa842ea08ea8858c0a14e80e
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-23 11:42:48 -0400

    Add check current page parts for prev parts and set color the same

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ACbuildHighlight
+
+https://github.com/gettalong/hexapdf.git
+
+Using Hexapdf to scan Aircraft build files for part numbers. Each page is
+scanned and part numbers are given unique colored highlights to correspond the
+written textual instruction to the drawings.
--- show_char_boxes.rb
+++ show_char_boxes.rb
@@ -6,18 +6,19 @@ require 'pry-byebug'
 require 'hexapdf'
 
 class ShowTextProcessor < HexaPDF::Content::Processor
-  attr_accessor :page_number, :color_key, :prev_color_key
+  attr_accessor :page_number, :color_key, :prev_parts, :prev_color
 
-  def initialize(page, page_number, prev_color_key = nil, color_key = {}) # text_list = []
+  def initialize(page, page_number, prev_parts = nil, prev_color = nil, color_key = {}) # text_list = []
     super()
     # @text_list_arr = text_list
     @canvas = page.canvas(type: :overlay)
-    @parts = %w[F1006C F1012A F1006D F1006B F1032L F1011C F1029L F1010C F1010A F1011E F1012E F1010C F1010 F1011 F1011A
+    @parts = %w[the F1006C F1012A F1006D F1006B F1032L F1011C F1029L F1010C F1010A F1011E F1012E F1010C F1010 F1011 F1011A
                 F1010CR F1010CL F1007L]
     @colors = %w[violet yellow maroon brown cyan darkgreen darkred gold deeppink olivedrab paleturquoise red green blue
                  orange]
     @color_key = color_key
-    @prev_color_key = prev_color_key
+    @prev_parts = prev_parts
+    @prev_color = prev_color
     @page_number = page_number
     @color_key['Page Number'] = @page_number
   end
@@ -29,6 +30,9 @@ class ShowTextProcessor < HexaPDF::Content::Processor
       puts 'invalid string'
     end
     return unless @parts.include?(part) # do nothing if part is not on current page
+
+    # check if part was on previous page
+    @color_key[part] = @prev_color.delete(part) if !@prev_parts.nil? and @prev_parts.include?(part)
 
     key_color(part, str) unless @color_key.key?(part) # return if part/color pair is in hash
 
@@ -58,17 +62,21 @@ class ShowTextProcessor < HexaPDF::Content::Processor
   alias show_text_with_positioning show_text
 end
 @prev_color = nil
+@color_key = nil
+@prev_parts = nil
 doc = HexaPDF::Document.open(ARGV.shift)
 doc.pages.each_with_index do |page, index|
   puts "Processing page #{index + 1}"
   unless @prev_color.nil?
-    parts = @prev_color.keys
-    parts.each do |part|
-      p part
-      # p @prev_color_key[part] = @prev_color[part]
-    end
+    p @prev_parts = @prev_color.keys
+    # @prev_color.each do |_key, _value|
+    #   parts.each do |part|
+    #     p part
+    #     p @prev_color[part] # = @prev_color[part]
+    #   end
+    # end
   end
-  processor = ShowTextProcessor.new(page, index)
+  processor = ShowTextProcessor.new(page, index, @prev_parts, @prev_color)
   # binding.pry
   page.process_contents(processor)
   @prev_color = processor.color_key
