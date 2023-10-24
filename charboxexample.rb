cfbfa4eaa64633b3020bde3ab5bd6e12c728be2c
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-24 18:55:52 -0400

    test files

--- charboxexample.rb
+++ charboxexample.rb
@@ -19,35 +19,38 @@ class ShowTextProcessor < HexaPDF::Content::Processor
   def initialize(page)
     super()
     @canvas = page.canvas(type: :overlay)
+    @all_text = []
+    @parts = {}
   end
 
   def show_text(str)
+    begin
+      @parts[str.select.with_index { |_, i| i.even? }.join] = str
+    rescue StandardError
+      nil
+    end
     boxes = decode_text_with_positioning(str)
     return if boxes.string.empty?
 
-    @canvas.line_width = 1
-    @canvas.stroke_color(224, 0, 0)
-    # Polyline for transformed characters
-    # boxes.each {|box| @canvas.polyline(*box.points).close_subpath.stroke}
-    # Using rectangles is faster but not 100% correct
-    boxes.each do |box|
-      x, y = *box.lower_left
-      tx, ty = *box.upper_right
-      @canvas.rectangle(x, y, tx - x, ty - y).stroke
-    end
+    @all_text << boxes
+  end
 
-    @canvas.line_width = 0.5
-    @canvas.stroke_color(0, 224, 0)
-    @canvas.polyline(*boxes.lower_left, *boxes.lower_right,
-                     *boxes.upper_right, *boxes.upper_left).close_subpath.stroke
+  def highlight
+    @all_text.each do |boxes|
+      @canvas.line_width = 0.5
+      @canvas.stroke_color(0, 224, 0)
+      @canvas.polyline(*boxes.lower_left, *boxes.lower_right,
+                       *boxes.upper_right, *boxes.upper_left).close_subpath.stroke
+    end
   end
   alias show_text_with_positioning show_text
 end
 
-doc = HexaPDF::Document.open(ARGV.shift)
+doc = HexaPDF::Document.open('06_10.pdf')
 doc.pages.each_with_index do |page, index|
   puts "Processing page #{index + 1}"
   processor = ShowTextProcessor.new(page)
   page.process_contents(processor)
+  processor.highlight
 end
 doc.write('charboxexample.pdf', optimize: true)
