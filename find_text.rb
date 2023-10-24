05599137525bbc6b7aba74f14129ccaa6abd20bd
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-24 13:08:42 -0400

    Remove old comments

--- find_text.rb
+++ find_text.rb
@@ -1,18 +1,3 @@
-# # Show Character Bounding Boxes
-#
-# This examples shows how to process the contents of a page. It finds all
-# characters on a page and surrounds them with their bounding box. Additionally,
-# all consecutive text runs are also surrounded by a box.
-#
-# The code provides two ways of generating the boxes. The commented part of
-# `ShowTextProcessor#show_text` uses a polyline since some characters may be
-# transforemd (rotated or skewed). The un-commented part uses rectangles which
-# is faster and correct for most but not all cases.
-#
-# Usage:
-# : `ruby show_char_boxes.rb INPUT.PDF`
-#
-
 require 'hexapdf'
 
 class FindTextProcessor < HexaPDF::Content::Processor
@@ -39,11 +24,3 @@ class FindTextProcessor < HexaPDF::Content::Processor
   end
   alias show_text_with_positioning show_text
 end
-
-# doc = HexaPDF::Document.open(ARGV.shift)
-# doc.pages.each_with_index do |page, index|
-#   puts "Processing page #{index + 1}"
-#   processor = ShowTextProcessor.new(page)
-#   page.process_contents(processor)
-# end
-# doc.write('show_char_boxes.pdf', optimize: true)
