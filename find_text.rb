3f6d76bd58667c81a808541f9bd0a7cc7d66134d
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-24 15:33:30 -0400

    Fix find_text to use new str output

--- find_text.rb
+++ find_text.rb
@@ -1,21 +1,26 @@
 require 'hexapdf'
+require_relative 'parts_list'
 
 class FindTextProcessor < HexaPDF::Content::Processor
+  include SectionParts
+
   attr_accessor :page_parts
 
   def initialize(page)
     super()
     @canvas = page.canvas(type: :overlay)
-    @parts = %w[F-1006C F-1012A F-1006D F-1006B F-1032L F-1011C F-1029-L F-1010C
-                F-1010A F-1011E F-1012E F-1010C F-1010 F-1011 F-1011A
-                F-1010C-R F-1010C-L F-1007-L]
+    # @parts = %w[F-1006C F-1012A F-1006D F-1006B F-1032L F-1011C F-1029-L F-1010C
+    #             F-1010A F-1011E F-1012E F-1010C F-1010 F-1011 F-1011A
+    #             F-1010C-R F-1010C-L F-1007-L]
+    @parts = getParts
     @page_parts = []
   end
 
   def show_text(str)
     begin
-      part = str.scan(/[-\w+]/).join # Converts utf-8 str to text string
-    rescue
+      # part = str.scan(/[-\w+]/).join # Converts utf-8 str to text string
+      part = str.select.with_index { |_, i| i.even? }.join
+    rescue StandardError
       nil
     end
     return unless @parts.include?(part) # do nothing if part is not on current page
