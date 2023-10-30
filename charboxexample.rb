bfe4da0a23f83b71415583cbc19dbbe7aa06dc84
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-30 14:07:42 -0400

    Add output of part position in array

--- charboxexample.rb
+++ charboxexample.rb
@@ -24,12 +24,12 @@ class ShowTextProcessor < HexaPDF::Content::Processor
     @all_text = []
     @parts = %w[F-1006C F-1012A F-1006D F-1006B F-1032L F-1011C F-1029-L F-1010C
                 F-1010A F-1011E F-1012E F-1010C F-1010 F-1011 F-1011A
-                F-1010C-R F-1010C-L F-1007-L]
+                F-1010C-R F-1010C-L F-1007-L VS-1014 VS-1014-R VS-1014-L]
     @boxes = nil
     @text = {}
   end
 
-  def show_text(str)
+  def show_text(str) # rubocop:disable Metrics/AbcSize, Metrics/PerceivedComplexity
     # test = decode_text_with_positioning(['T', 8.44, 'ri', 4.049, 'm '])
     begin
       part_number_string = str.select.with_index { |_, i| i.even? }.join
@@ -43,16 +43,12 @@ class ShowTextProcessor < HexaPDF::Content::Processor
     @all_text << boxes
     return if part_number_string.nil?
 
-    b = part_number_string.split(' ')
-    part_number = nil
-    b.each do |word|
-      arr = [word]
-      part_number = @parts & arr
-    end
-    nil if part_number.nil?
+    return unless part_number_string.split(' ').count > 1
 
-    # positions = part_number_string.enum_for(:scan, /#{part_number}/).map { Regexp.last_match.begin(0) }
-    # puts positions
+    @parts.each do |part_number|
+      positions = part_number_string.enum_for(:scan, /#{part_number}/).map { Regexp.last_match.begin(0) }
+      p positions unless positions.empty?
+    end
   end
 
   def highlight
@@ -70,7 +66,7 @@ class ShowTextProcessor < HexaPDF::Content::Processor
   alias show_text_with_positioning show_text
 end
 
-doc = HexaPDF::Document.open('06_10.pdf')
+doc = HexaPDF::Document.open('no_first_page.pdf')
 doc.pages.each_with_index do |page, index|
   puts "Processing page #{index + 1}"
   processor = ShowTextProcessor.new(page)
