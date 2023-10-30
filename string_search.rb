24c8d22b4b00e1e11bfeb7db7837ddad2eecdce1
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-10-30 16:43:59 -0400

    Test search string for part with index

---
+++ string_search.rb
@@ -0,0 +1,24 @@
+str = ['t', 8.771, 'w', -8.771, 'o', 4.722, ' ', -4.049, 'VS', -12.489, '-', 12.479, '1', -8.098, '0', 4.712,
+       '1', -8.098, '4', 4.712, ' ', -4.049, 'R', 4.049, 'e']
+
+part = 'VS-1014'
+@part_location = []
+part_arr = part.split('')
+
+@str_search = []
+str.each_with_index do |string, index|
+  @str_search << string
+  @part_location << index
+  next unless @str_search.length > (part.length * 2)
+
+  @str_search.shift
+  @part_location.shift
+
+  word = @str_search.select.with_index { |_, i| i.even? }.join
+  begin
+    # p @part_location if word.match(/#{part}/)
+    p @part_location if word.match(part) && word[0] != ' '
+  rescue StandardError
+    puts 'Could not find'
+  end
+end
