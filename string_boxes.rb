da49e3177b2ad8c3c4787a41e36a3ed2428674fc
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-01 12:59:21 -0400

    Add assign_color for current page only. no look back

--- string_boxes.rb
+++ string_boxes.rb
@@ -13,9 +13,12 @@ class StringBoxesProcessor < HexaPDF::Content::Processor
     # @parts = %w[F-1006C F-1012A F-1006D F-1006B F-1032L F-1011C F-1029-L F-1010C
     #             F-1010A F-1011E F-1012E F-1010C F-1010 F-1011 F-1011A
     #             F-1010C-R F-1010C-L F-1007-L]
+    @colors = %w[cyan darkgreen gold deeppink olivedrab paleturquoise red green blue orange]
+
     @parts = getParts
     @str_boxes = {}
     @page_parts = {}
+    @color_key = {}
   end
 
   def show_text(str) # /
@@ -39,18 +42,39 @@ class StringBoxesProcessor < HexaPDF::Content::Processor
         next if positions.nil? || positions.empty?
 
         positions.each do |pos|
-          @page_parts[string] = [] unless @page_parts.key?(string)
+          @page_parts[string] = {} unless @page_parts.key?(string)
+          @page_parts[string][part_number] = [] unless @page_parts[string].key?(part_number)
 
-          @page_parts[string].push(value.cut(pos, (pos + part_number.length)))
+          @page_parts[string][part_number].push(value.cut(pos, (pos + part_number.length)))
         end
       end
       # end
     end
   end
 
+  def assign_color(list)
+    p = []
+
+    list.each do |_key, value|
+      value.each do |k, _v|
+        p << k
+      end
+    end
+    unique_parts = p.uniq
+    unique_parts.each do |part|
+      @colors.each do |color|
+        next if @color_key.values.include?(color)
+
+        break @color_key[part] = color
+      end
+    end
+  end
+
   def color(encodes)
     encodes.each do |_string, boxes|
-      boxes.each do |b|
+      boxes.each_with_index do |b, i|
+        next if i.zero?
+
         b.each do |box|
           x, y = *box.lower_left
           tx, ty = *box.upper_right
@@ -71,6 +95,7 @@ doc.pages.each_with_index do |page, index|
   str_boxes = processor.str_boxes
   processor.match(str_boxes)
   page_parts = processor.page_parts
+  processor.assign_color(page_parts)
   processor.color(page_parts)
 end
 doc.write('show_char_boxes.pdf', optimize: true)
